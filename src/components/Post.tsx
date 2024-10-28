9cdb738143cbfc043d97445933e7e63c8c1a1082
quoc-bao-dev <baotqps36791@fpt.edu.vn>
2024-10-28 22:56:18 +0700

    fea: delete post

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,17 +1,30 @@
 import { format } from "date-fns"
 import { TPost } from "../interfaces/post"
+import { deletePost, getAllPost } from "../services/postService"
+import { setPosts } from "../features/postSlice"
+import { useDispatch } from "react-redux"
 
 type TPostProps = TPost
 
-const Post = ({ name, avatar, title, content, createdAt }: TPostProps) => {
+const Post = ({ name, avatar, title, content, createdAt, id }: TPostProps) => {
+    const dispatch = useDispatch()
+
+    const handleDelete = async () => {
+        await deletePost(id)
+        getAllPost().then(res => dispatch(setPosts([...res.data])))
+        alert("Post deleted successfully")
+    }
     return (
-        <div className="p-2 bg-white rounded-lg shadow-md">
-            <div className="flex gap-2 items-center">
-                <img src={avatar} className="w-10 h-10 rounded-full" alt="" />
-                <div>
-                    <p className="text-sm font-semibold">{name}</p>
-                    <p className="text-xs text-gray-600">{format(new Date(createdAt), "dd/MM/yyyy")}</p>
+        <div className="p-4 bg-white rounded-lg shadow-md">
+            <div className="flex justify-between items-center">
+                <div className="flex gap-2 items-center">
+                    <img src={avatar} className="w-10 h-10 rounded-full" alt="" />
+                    <div>
+                        <p className="text-sm font-semibold">{name}</p>
+                        <p className="text-xs text-gray-600">{format(new Date(createdAt), "dd/MM/yyyy")}</p>
+                    </div>
                 </div>
+                <button onClick={handleDelete} className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600">Delete</button>
             </div>
 
             <div className="pt-2">
@@ -23,3 +36,4 @@ const Post = ({ name, avatar, title, content, createdAt }: TPostProps) => {
 }
 
 export default Post
+
--- src/services/postService.ts
+++ src/services/postService.ts
@@ -5,3 +5,5 @@ const BASE_URL = "https://671f9751e7a5792f052ed19f.mockapi.io/api/v1/posts"
 export const getAllPost = () => axios.get(BASE_URL)
 
 export const createPost = (data: Omit<TPost, "id" | "createdAt">) => axios.post(BASE_URL, data)
+
+export const deletePost = (id: string) => axios.delete(`${BASE_URL}/${id}`)
